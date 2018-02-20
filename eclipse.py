bcec9cf5904d1dee3d454c6a361a3f852cf9a67c
stephtdouglas <stephanietdouglas@gmail.com>
2018-02-20 15:37:26 -0500

    Added second object light curve, plot title is star name

--- eclipse.py
+++ eclipse.py
@@ -19,28 +19,28 @@ import numpy as np
 from bokeh.io import curdoc
 from bokeh.layouts import row, widgetbox
 from bokeh.models import ColumnDataSource
-from bokeh.models.widgets import Slider, TextInput
+from bokeh.models.widgets import Slider, TextInput, Dropdown
 from bokeh.plotting import figure
 
 from kepio import kepio
 
-t,f,e = kepio("kplr011517719-2013098041711_llc.fits.txt")
+lightcurves = {}
+t840, f840, e840 = kepio("kplr011517719-2013098041711_llc.fits.txt")
+lightcurves["Kepler 840"] = {"t":t840,"f":f840,"e":e840}
+t488, f488, e488 = kepio("kplr010904857-2013131215648_llc.fits.txt")
+lightcurves["Kepler 488"] = {"t":t488,"f":f488,"e":e488}
 
-x0 = t
-y = f
 
 # Set up data
-# N = 500
-# x0 = np.linspace(0, 10*np.pi, N)
+s = "Kepler 840"
+x0,y = lightcurves[s]["t"],lightcurves[s]["f"]
 period = 2.0
 x = (x0 % period) / period
-# errs = np.random.randn(N)*0.1
-# y = np.sin(x0) + np.sin(x0*2 + np.pi/3) + errs
 source = ColumnDataSource(data=dict(x=x, y=y))
 
 
 # Set up plot
-plot = figure(plot_height=400, plot_width=600, title="My Kepler Light Curve",
+plot = figure(plot_height=400, plot_width=600, title=s,
               tools="crosshair,pan,reset,save,wheel_zoom"
               ,x_range=[0,1]
               # y_range=[-2.5, 2.5])
@@ -50,34 +50,41 @@ plot.circle('x', 'y', source=source, size=10, alpha=0.6)
 
 
 # Set up widgets
-text = TextInput(title="title", value='My Kepler Light Curve')
-period = Slider(title="period", value=2, start=1, end=3, step=0.0001)
-T0 = Slider(title="T_0", value=x[0], start=x[0], end=x[100], step=x[1]-x[0])
+text = TextInput(title="Period (Refine)", value="2")
+period = Slider(title="Period", value=2, start=1, end=10, step=0.0001)
+T0 = Slider(title="Time_0", value=x[0], start=x[0], end=x[100], step=x[1]-x[0])
+
+menu = [("Kepler 840", "Kepler 840"), ("Kepler 488", "Kepler 488")]
+star = Dropdown(label="Object", button_type="warning", menu=menu)
 
 # Set up callbacks
 def update_title(attrname, old, new):
-    plot.title.text = text.value
+    plot.title.text = star.value
 
-text.on_change('value', update_title)
+star.on_change('value', update_title)
 
 def update_data(attrname, old, new):
+
+    # Get the current lightcurve
+    s = star.value
 
     # Get the current slider values
     p = period.value
     t = T0.value
 
     # Generate the new curve
+    x0, y = lightcurves[s]["t"], lightcurves[s]["f"]
     x = ((x0 - t) % p) / p
     # y = np.sin(x0) + errs
 
     source.data = dict(x=x, y=y)
 
-for w in [period, T0]:
+for w in [star, period, T0]:
     w.on_change('value', update_data)
 
 
 # Set up layouts and add to document
-inputs = widgetbox(text, period, T0)
+inputs = widgetbox(text, star, period, T0)
 
 curdoc().add_root(row(inputs, plot, width=1000))
 curdoc().title = "Eclipse"
